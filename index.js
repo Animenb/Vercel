297867494f0904da4facb6fb256d2909c8527738
Animenb <120386240+Animenb@users.noreply.github.com>
2023-02-17 23:44:14 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
+const { google } = require("googleapis");
+const { OAuth2 } = google.auth;
+const oauth2Client = new OAuth2(
+  YOUR_CLIENT_ID,
+  YOUR_CLIENT_SECRET,
+  YOUR_REDIRECT_URI
+);
 
 async function handleRequest(request) {
   const url = new URL(request.url);
@@ -22,14 +26,15 @@ class googleDrive {
       session.token_expiry || new Date().toISOString()
     );
     token_expiry = token_expiry.getTime();
-    this.config = {
+    this.oauth2Client = oauth2Client;
+    this.oauth2Client.setCredentials({
       access_token: session.access_token,
-      client_id: session.client_id,
-      client_secret: session.client_secret,
       refresh_token: session.refresh_token,
-      token_expiry: token_expiry,
-    };
+      token_type: "Bearer",
+      expiry_date: token_expiry,
+    });
   }
+
   async downloadAPI(range, session) {
     if (session.transcoded == true && session.cookie) {
       let requestOption = {
@@ -44,67 +49,28 @@ class googleDrive {
       headers.append("Access-Control-Allow-Origin", "*");
       headers.set("Content-Disposition", "inline");
       headers.set("Access-Control-Allow-Headers", "*");
-      headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+      headers.set(
+        "Cache-Control",
+        "no-cache, no-store, must-revalidate"
+      );
       headers.set("pragma", "no-cache");
       return resp;
     } else {
-      await this.setAccessToken();
-      let requestOption = {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${this.config.access_token}`,
-          Range: range,
-        },
-      };
-      let resp = await fetch(session.url, requestOption);
+      await this.oauth2Client.getRequestHeaders().then((headers) => {
+        headers.Range = range;
+        return headers;
+      });
+      let resp = await fetch(session.url, { headers });
       let { headers } = (resp = new Response(resp.body, resp));
       headers.append("Access-Control-Allow-Origin", "*");
       headers.set("Content-Disposition", "inline");
       headers.set("Access-Control-Allow-Headers", "*");
-      headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+      headers.set(
+        "Cache-Control",
+        "no-cache, no-store, must-revalidate"
+      );
       headers.set("pragma", "no-cache");
       return resp;
     }
-  }
-
-  async setAccessToken() {
-    if (
-      this.config.token_expiry == undefined ||
-      this.config.token_expiry < Date.now()
-    ) {
-      const obj = await this.getAccessToken();
-      if (obj.access_token != undefined) {
-        this.config.access_token = obj.access_token;
-        this.config.token_expiry = obj.token_expiry;
-      }
-    }
-  }
-
-  async getAccessToken() {
-    const url = "https://www.googleapis.com/oauth2/v4/token";
-    const headers = {
-      "Content-Type": "application/x-www-form-urlencoded",
-    };
-    const post_data = {
-      client_id: this.config.client_id,
-      client_secret: this.config.client_secret,
-      refresh_token: this.config.refresh_token,
-      grant_type: "refresh_token",
-    };
-    let requestOption = {
-      method: "POST",
-      headers: headers,
-      body: this.enQuery(post_data),
-    };
-    const response = await fetch(url, requestOption);
-    return await response.json();
-  }
-
-  enQuery(data) {
-    const ret = [];
-    for (let d in data) {
-      ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
-    }
-    return ret.join("&");
   }
 }
