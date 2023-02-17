d30de91d99128f2561e5ffe944840b90d4eb1019
Animenb <120386240+Animenb@users.noreply.github.com>
2023-02-17 23:52:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-const { google } = require("googleapis");
-const { OAuth2 } = google.auth;
-const oauth2Client = new OAuth2(
-  YOUR_CLIENT_ID,
-  YOUR_CLIENT_SECRET,
-  YOUR_REDIRECT_URI
-);
+// Import necessary modules
+const fetch = require("node-fetch");
 
-async function handleRequest(request) {
-  const url = new URL(request.url);
+// Define your serverless function
+module.exports = async (req, res) => {
+  const url = new URL(req.url);
   const path = url.pathname;
   if (!path.toLowerCase().startsWith("/api")) {
-    return new Response(
-      "libDrive for Cloudflare doesn't work on its own. It is only an extention to the backend."
+    res.status(400).send(
+      "libDrive for Cloudflare doesn't work on its own. It is only an extension to the backend."
     );
   } else if (path.toLowerCase().startsWith("/api/v1/download")) {
-    const session = JSON.parse(atob(url.searchParams.get("session")));
+    const session = JSON.parse(Buffer.from(url.searchParams.get("session"), "base64").toString("ascii"));
     const drive = new googleDrive(session);
-    return drive.downloadAPI(request.headers.get("Range") || "", session);
+    const range = req.headers.range || "";
+    const resp = await drive.downloadAPI(range, session);
+    res.set(resp.headers);
+    res.status(resp.status);
+    res.send(await resp.buffer());
   }
-}
+};
 
+// Define the googleDrive class
 class googleDrive {
   constructor(session) {
-    let token_expiry = new Date(
-      session.token_expiry || new Date().toISOString()
-    );
+    let token_expiry = new Date(session.token_expiry || new Date().toISOString());
     token_expiry = token_expiry.getTime();
-    this.oauth2Client = oauth2Client;
-    this.oauth2Client.setCredentials({
+    this.config = {
       access_token: session.access_token,
+      client_id: session.client_id,
+      client_secret: session.client_secret,
       refresh_token: session.refresh_token,
-      token_type: "Bearer",
-      expiry_date: token_expiry,
-    });
+      token_expiry: token_expiry,
+    };
   }
-
   async downloadAPI(range, session) {
     if (session.transcoded == true && session.cookie) {
       let requestOption = {
@@ -49,28 +47,38 @@ class googleDrive {
       headers.append("Access-Control-Allow-Origin", "*");
       headers.set("Content-Disposition", "inline");
       headers.set("Access-Control-Allow-Headers", "*");
-      headers.set(
-        "Cache-Control",
-        "no-cache, no-store, must-revalidate"
-      );
+      headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
       headers.set("pragma", "no-cache");
       return resp;
     } else {
-      await this.oauth2Client.getRequestHeaders().then((headers) => {
-        headers.Range = range;
-        return headers;
-      });
-      let resp = await fetch(session.url, { headers });
+      await this.setAccessToken();
+      let requestOption = {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${this.config.access_token}`,
+          Range: range,
+        },
+      };
+      let resp = await fetch(session.url, requestOption);
       let { headers } = (resp = new Response(resp.body, resp));
       headers.append("Access-Control-Allow-Origin", "*");
       headers.set("Content-Disposition", "inline");
       headers.set("Access-Control-Allow-Headers", "*");
-      headers.set(
-        "Cache-Control",
-        "no-cache, no-store, must-revalidate"
-      );
+      headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
       headers.set("pragma", "no-cache");
       return resp;
     }
   }
-}
+
+  async setAccessToken() {
+    if (
+      this.config.token_expiry == undefined ||
+      this.config.token_expiry < Date.now()
+    ) {
+      const obj = await this.getAccessToken();
+      if (obj.access_token != undefined) {
+        this.config.access_token = obj.access_token;
+        this.config.token_expiry = obj.token_expiry;
+      }
+    }
+  }
